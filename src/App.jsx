7948b77b388ca1645207319c5cde643005d73b9d
dheerajk-p <dkp212000@gmail.com>
2025-08-11 16:50:21 +0530

    feat : initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+import Midweek from './components/midweek'
+
+function App() {
+  return (
+    <div className='max-w-[1440px] mx-auto'>
+      <Midweek />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Midweek.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Midweek () {
+  return (
+    
+    <div className='max-w-[1275px] mx-auto'>
+      <div className='flex items-center gap-10' >
+        <div> <img src="public\midweekimage.jpg" className='w-[432px] h-[432px] rounded-[20px] object-cover' alt="meditationimage" /></div>
+        <div className='flex flex-col gap-4'>
+          <h1>Midweek Calm – 20% Off All Bookings</h1>
+        </div>
+      </div>
+    </div>
+    
+  )
+}
+
+export default Midweek
